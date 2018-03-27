475f8233c75017838424841f53a2adc6b06ed7bf
twicoder <renqingwei123@163.com>
2018-03-27 21:54:58 +0800

    Base project with hello world

---
+++ src/main/java/com/chinwe/springbootdemo1/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.chinwe.springbootdemo1.controller;
+
+import com.chinwe.springbootdemo1.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @RequestMapping("/")
+    public String home(Model model){
+        model.addAttribute("users", userRepo.findAll());
+        return "index";
+    }
+}
